07b0fbc128d1fa2bf52637eac08cef6ef3a18980
Saloo2000 <salman.saloo2000@gmail.com>
2025-07-23 18:56:15 +0500

    first commit

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Logo() {
   return (
-          <a href="/">
+          <Link to="/">
               <div>
             <h2 className='font-helvetica font-bold text-[1.4vw]'>RAYDIANT WEB</h2>
             <p className='font-montserrat font-semibold text-[.4vw] -mt-1'>Lighting Up the Digital Horizon</p>
             </div>
-          </a>
+          </Link>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='text-[20px] flex gap-6 '>
-      <div className='relative group'><a className='py-2' href="/RaydiantWebs/work">Work</a><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
-      <div className='relative group'><a className='py-2' href="/RaydiantWebs/services">Services</a><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
-      <div className='relative group'><a className='py-2' href="/RaydiantWebs/experties">Expertise</a><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
-      <div className='relative group'><a className='py-2' href="#">Stories</a><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
-      <div className='relative group'><a className='py-2' href="#">Careers</a><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
+      <div className='relative group'><Link className='py-2' href="/RaydiantWebs/work">Work</Link><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
+      <div className='relative group'><Link className='py-2' href="/RaydiantWebs/services">Services</Link><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
+      <div className='relative group'><Link className='py-2' href="/RaydiantWebs/experties">Expertise</Link><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
+      <div className='relative group'><Link className='py-2' href="#">Stories</Link><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
+      <div className='relative group'><Link className='py-2' href="#">Careers</Link><span class="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span></div>
     </div>
   )
 }
